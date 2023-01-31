cfb8e6ad69c990db3a3cd1f833108e5363455fb9
eduarda.santana <eduarda.santana@jeaholding.com.br>
2023-01-31 14:13:52 -0300

    finishing the header adaptation

--- wefit-test-js-assets/wefit-test/js/main.js
+++ wefit-test-js-assets/wefit-test/js/main.js
@@ -1,3 +1,4 @@
+// MENU
 const listButtonMenu = document.querySelector(".btn-group-vertical");
 listButtonMenu.setAttribute(
   "style",
@@ -10,3 +11,26 @@ for (let i = 0; i < listButtonMenu.children.length; i++) {
     "width: max-content; flex: none; border-radius: 0.25rem; margin-right: 4px"
   );
 }
+
+// HEADER
+const hedaerContainer = document.querySelector(".jumbotron");
+hedaerContainer.setAttribute(
+  "style",
+  "background-color: #6c757d; color: white; display: flex; flex-direction: column; align-items: end; width: 100%"
+);
+
+const hedaerH1 = hedaerContainer.querySelector("h1");
+hedaerH1.setAttribute("style", "display: flex; justify-content: end");
+
+const hedaerP = hedaerContainer.querySelectorAll("p");
+hedaerP[0].setAttribute("style", "display: flex; justify-content: end");
+hedaerP[1].setAttribute(
+  "style",
+  "display: flex; justify-content: end; text-align: right;"
+);
+
+const hedaerHR = hedaerContainer.querySelector(".my-4");
+hedaerHR.setAttribute("style", "width: 100%;");
+
+const hedaerA = hedaerContainer.querySelector("a");
+hedaerA.setAttribute("style", "background-color: green; border: none");
